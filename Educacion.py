d5b0069f58aeeaa52f7e7cfd393ff9a022789d26
Erwin <75356078+Cuadernin@users.noreply.github.com>
2021-04-17 00:51:43 -0500

    Version 1

--- Educacion.py
+++ Educacion.py
@@ -4,11 +4,9 @@ def numeros(n):
      ">>>>>>>>>>>>>>>>>> EJEMPLO DE USO DE API PARA SOLICITAR DATOS INTERESANTES DE UN NUMERO <<<<<<<<<<<<<<<<<<<"
     url=f"https://numbersapi.p.rapidapi.com/{n}/math"
     querystring={"fragment":"true","json":"´true"}
-    headers={
-        'x-rapidapi-key': "5c7d84061amshb6ada123e8de3fcp104033jsne3d05d482678",
-        'x-rapidapi-host': "numbersapi.p.rapidapi.com"
-        }
+    headers={'x-rapidapi-key': "5c7d84061amshb6ada123e8de3fcp104033jsne3d05d482678",'x-rapidapi-host': "numbersapi.p.rapidapi.com"}
     response=requests.request("GET", url, headers=headers, params=querystring)
+     
     respuesta=response.json()
     respuesta=respuesta['text']
     respuesta=GoogleTranslator(source='auto',target="es").translate(text=respuesta)
