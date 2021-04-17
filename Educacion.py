e8b59637246a58731420c2bd13317e5fd6343643
Erwin <75356078+Cuadernin@users.noreply.github.com>
2021-04-17 00:49:44 -0500

    Version 1

--- Educacion.py
+++ Educacion.py
@@ -1,6 +1,7 @@
 import requests
 from deep_translator import GoogleTranslator
 def numeros(n):
+     ">>>>>>>>>>>>>>>>>> EJEMPLO DE USO DE API PARA SOLICITAR DATOS INTERESANTES DE UN NUMERO <<<<<<<<<<<<<<<<<<<"
     url=f"https://numbersapi.p.rapidapi.com/{n}/math"
     querystring={"fragment":"true","json":"´true"}
     headers={
@@ -14,14 +15,5 @@ def numeros(n):
     return respuesta.capitalize()+"."
 
 def rand(n):
-    url=f"https://numbersapi.p.rapidapi.com/{n}/trivia"
-    querystring={"fragment":"true","notfound":"floor","json":"true"}
-    headers = {
-    'x-rapidapi-key': "5c7d84061amshb6ada123e8de3fcp104033jsne3d05d482678",
-    'x-rapidapi-host': "numbersapi.p.rapidapi.com"
-    }
-    response=requests.request("GET", url, headers=headers, params=querystring)
-    respuesta=response.json()
-    respuesta=respuesta['text']
-    respuesta=GoogleTranslator(source='auto',target="es").translate(text=respuesta)
+     ">>>>>>>>>>>>>>>>>> INGRESE UNA API COMO LA ANTERIOR <<<<<<<<<<<<<<<<<<<"
     return respuesta.capitalize()+"."
