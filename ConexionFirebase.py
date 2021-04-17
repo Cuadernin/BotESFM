92159b4a7bd8bc522e7b7fb8e43dc1d3990e23c1
Erwin <75356078+Cuadernin@users.noreply.github.com>
2021-04-17 00:11:40 -0500

    Update ConexionFirebase.py

--- ConexionFirebase.py
+++ ConexionFirebase.py
@@ -3,23 +3,14 @@ from firebase_admin import credentials
 from firebase_admin import db
 import firebase_admin
 def conexion(chat_id,username,texto,maestro):
-    base=firebase.FirebaseApplication("https://base-8fb74-default-rtdb.firebaseio.com/",None)
+    base=firebase.FirebaseApplication("URL",None)
     datos={"chat_id":chat_id,"Nombre":username,"mensaje":texto}
-    resultado=base.post(f'/profesores/{maestro}',datos)
+    resultado=base.post(f'/Collection/{maestro}',datos)
     return resultado
 
 def lecturaR(maestro):
-    cred=credentials.Certificate('C:\\Users\\Erwin Eduardo G.C\\Documents\\PYTHON\\Bot\\base-8fb74-firebase.json')
-    firebase_admin.initialize_app(cred,{'databaseURL':"https://base-8fb74-default-rtdb.firebaseio.com/"})
-    ref=db.reference(f"/profesores/{maestro}")
+    cred=credentials.Certificate('Collection')
+    firebase_admin.initialize_app(cred,{'databaseURL':"URL"})
+    ref=db.reference(f"/Collection/{maestro}")
     ref=ref.get()
     return ref
-
-def lecturaT():
-    base=firebase.FirebaseApplication("https://base-8fb74-default-rtdb.firebaseio.com/",None)
-    leer=base.get('/profesores/','')
-    return leer
-"""
-base=firebase.FirebaseApplication("https://base-8fb74-default-rtdb.firebaseio.com/",None)
-    leer=base.get(f'/profesores/{maestro}','')
-    """
