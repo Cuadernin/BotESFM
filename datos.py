e9f3f1d094bcb4f59703547a0f0a93b6210abb50
Erwin <75356078+Cuadernin@users.noreply.github.com>
2021-04-17 00:54:46 -0500

    Version 1

--- datos.py
+++ datos.py
@@ -6,18 +6,6 @@ def base():
         texto=f.read()
     return texto
 
-def txtprofesores(maestro):
-    leer=lecturaR(maestro)
-    txt=open("profesores_ESFM.txt","w+")
-    for key in leer:
-        nombre=leer[key]['Nombre'].upper()
-        mensaje=leer[key]['mensaje']
-        mensajes=f'{nombre}: {mensaje}\n\n'
-        txt.write(mensajes)
-    txt.close()
-    file1=open("profesores_ESFM.txt","r+")
-    return file1
-
 def txtescuela():
     leer=lecturaSug()
     txt=open("sugerencias_escuela.txt","w+")
@@ -29,3 +17,7 @@ def txtescuela():
     txt.close()
     file2=open("sugerencias_escuela.txt","r+")
     return file2
+
+def txtprofesores(maestro):
+     ">>>>>>>>>>>>>>>>>> SIMILAR A LA ANTERIOR FUNCION <<<<<<<<<<<<<<<<<<<"
+    return file1
