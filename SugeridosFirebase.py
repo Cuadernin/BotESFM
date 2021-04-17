6a5f0c5795cb3df5d327ec21b37d778f9b4a7ed1
Erwin <75356078+Cuadernin@users.noreply.github.com>
2021-04-17 00:58:13 -0500

    Version 1

--- SugeridosFirebase.py
+++ SugeridosFirebase.py
@@ -1,16 +1,19 @@
 from firebase import firebase
 def sug(chat_id,username,texto):
+    ">>>>>>>>>>>>>>>>>> MANERA SENCILLA DE INGRESAR DATOS A UNA BASE DE DATOS DE FIREBASE <<<<<<<<<<<<<<<<<<<"
     base=firebase.FirebaseApplication("URL",None)
     datos={"chat_id":chat_id,"Nombre":username,"mensaje":texto}
     resultado=base.post('Collection',datos)
     return resultado
 
 def lecturaSug():
+      ">>>>>>>>>>>>>>>>>> MANERA SENCILLA DE OBTENER DATOS DE UNA BASE DE DATOS DE FIREBASE <<<<<<<<<<<<<<<<<<<"
     base=firebase.FirebaseApplication("URL",None)
     leer=base.get('Collection','')
     return leer
 
 def sugerenciasbot(chat_id,username,texto):
+    ">>>>>>>>>>>>>>>>>> MANERA SENCILLA DE INGRESAR DATOS A UNA BASE DE DATOS DE FIREBASE <<<<<<<<<<<<<<<<<<<"
     base=firebase.FirebaseApplication("URL",None)
     datos={"chat_id":chat_id,"Nombre":username,"mensaje":texto}
     resultado=base.post('Collection',datos)
