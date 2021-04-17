5cebd864aa962003d1b02ecd8d1569572a238c76
Erwin <75356078+Cuadernin@users.noreply.github.com>
2021-04-17 00:13:56 -0500

    Update datos.py

--- datos.py
+++ datos.py
@@ -1,31 +1,31 @@
-from ConexionFirebase import lecturaR,lecturaT
+from ConexionFirebase import lecturaR
 from SugeridosFirebase import lecturaSug
 
 def base():
-    with open("C:\\Users\\Erwin Eduardo G.C\\Documents\\PYTHON\\Bot\\BASEDEDATOS.txt","r",encoding="utf-8") as f:
+    with open("BASEDEDATOS.txt","r",encoding="utf-8") as f:
         texto=f.read()
     return texto
 
 def txtprofesores(maestro):
     leer=lecturaR(maestro)
-    txt=open("C:\\Users\\Erwin Eduardo G.C\\Documents\\PYTHON\\Bot\\profesores_ESFM.txt","w+")
+    txt=open("profesores_ESFM.txt","w+")
     for key in leer:
         nombre=leer[key]['Nombre'].upper()
         mensaje=leer[key]['mensaje']
         mensajes=f'{nombre}: {mensaje}\n\n'
         txt.write(mensajes)
     txt.close()
-    file1=open("C:\\Users\\Erwin Eduardo G.C\\Documents\\PYTHON\\Bot\\profesores_ESFM.txt","r+")
+    file1=open("profesores_ESFM.txt","r+")
     return file1
 
 def txtescuela():
     leer=lecturaSug()
-    txt=open("C:\\Users\\Erwin Eduardo G.C\\Documents\\PYTHON\\Bot\\sugerencias_escuela.txt","w+")
+    txt=open("sugerencias_escuela.txt","w+")
     for key in leer:
         nombre=leer[key]['Nombre'].upper()
         mensaje=leer[key]['mensaje']
         mensajes=f'{nombre}: {mensaje}\n\n'
         txt.write(mensajes)
     txt.close()
-    file2=open("C:\\Users\\Erwin Eduardo G.C\\Documents\\PYTHON\\Bot\\sugerencias_escuela.txt","r+")
+    file2=open("sugerencias_escuela.txt","r+")
     return file2
