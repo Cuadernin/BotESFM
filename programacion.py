6dcdfb57ea211dafb42c560e6e446db30aafa862
Erwin <75356078+Cuadernin@users.noreply.github.com>
2021-04-17 00:14:14 -0500

    Update programacion.py

--- programacion.py
+++ programacion.py
@@ -1,4 +1,4 @@
 def plataforma():
-    with open("C:\\Users\\Erwin Eduardo G.C\\Documents\\PYTHON\\Bot\\Programacion.txt","r",encoding="utf-8") as f:
+    with open("Programacion.txt","r",encoding="utf-8") as f:
         texto=f.read()
     return texto
