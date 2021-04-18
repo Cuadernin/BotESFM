ed9de253fc3c969028e63d698cacf4c6db69fb09
Erwin <75356078+Cuadernin@users.noreply.github.com>
2021-04-18 18:27:02 -0500

    Version 1

--- ConexionFirebase.py
+++ ConexionFirebase.py
@@ -4,15 +4,25 @@ from firebase_admin import db
 import firebase_admin
 def conexion(chat_id,username,texto,maestro):
      ">>>>>>>>>>>>>>>>>> MANERA RAPIDA PARA INGRESAR DATOS A UNA BASE DE DATOS DE FIREBASE <<<<<<<<<<<<<<<<<<<"
-    base=firebase.FirebaseApplication("URL",None)
-    datos={"chat_id":chat_id,"Nombre":username,"mensaje":texto}
-    resultado=base.post(f'/Collection/{maestro}',datos)
-    return resultado
-
+     if not firebase_admin._apps:
+         base=firebase.FirebaseApplication("URL",None)
+         datos={"chat_id":chat_id,"Nombre":username,"mensaje":texto}
+         resultado=base.post(f'/Collection/{maestro}',datos)
+         return resultado
+     else:
+         datos={"chat_id":chat_id,"Nombre":username,"mensaje":texto}
+         resultado=base.post(f'/Collection/{maestro}',datos)
+         return resultado
+          
 def lecturaR(maestro):
      ">>>>>>>>>>>>>>>>>> MANERA OPTIMA PARA SOLICITAR DATOS DE UNA BASE DE DATOS DE FIREBASE <<<<<<<<<<<<<<<<<<<"
-    cred=credentials.Certificate('Collection')
-    firebase_admin.initialize_app(cred,{'databaseURL':"URL"})
-    ref=db.reference(f"/Collection/{maestro}")
-    ref=ref.get()
-    return ref
+     if not firebase_admin._apps:
+          cred=credentials.Certificate('Collection')
+         firebase_admin.initialize_app(cred,{'databaseURL':"URL"})
+         ref=db.reference(f"/Collection/{maestro}")
+         ref=ref.get()
+         return ref
+     else: 
+         ref=db.reference(f"/Collection/{maestro}")
+         ref=ref.get()
+         return ref
