7c52d1d8852a9f50a76e1c7cb7e0f003c32e23bc
Erwin <75356078+Cuadernin@users.noreply.github.com>
2021-04-18 18:30:36 -0500

    Version 1

--- SugeridosFirebase.py
+++ SugeridosFirebase.py
@@ -1,20 +1,37 @@
 from firebase import firebase
+import firebase_admin
+from firebase_admin import credentials
+
 def sug(chat_id,username,texto):
     ">>>>>>>>>>>>>>>>>> MANERA SENCILLA DE INGRESAR DATOS A UNA BASE DE DATOS DE FIREBASE <<<<<<<<<<<<<<<<<<<"
-    base=firebase.FirebaseApplication("URL",None)
-    datos={"chat_id":chat_id,"Nombre":username,"mensaje":texto}
-    resultado=base.post('Collection',datos)
-    return resultado
+    if not firebase_admin._apps:
+        base=firebase.FirebaseApplication("URL",None)
+        datos={"chat_id":chat_id,"Nombre":username,"mensaje":texto}
+        resultado=base.post('Collection',datos)
+        return resultado
+    else:
+        datos={"chat_id":chat_id,"Nombre":username,"mensaje":texto}
+        resultado=base.post('Collection',datos)
+        return resultado
 
 def lecturaSug():
       ">>>>>>>>>>>>>>>>>> MANERA SENCILLA DE OBTENER DATOS DE UNA BASE DE DATOS DE FIREBASE <<<<<<<<<<<<<<<<<<<"
-    base=firebase.FirebaseApplication("URL",None)
-    leer=base.get('Collection','')
-    return leer
+    if not firebase_admin._apps:
+        base=firebase.FirebaseApplication("URL",None)
+        leer=base.get('Collection','')
+        return leer
+    else:
+        leer=base.get('Collection','')
+        return leer
 
 def sugerenciasbot(chat_id,username,texto):
     ">>>>>>>>>>>>>>>>>> MANERA SENCILLA DE INGRESAR DATOS A UNA BASE DE DATOS DE FIREBASE <<<<<<<<<<<<<<<<<<<"
-    base=firebase.FirebaseApplication("URL",None)
-    datos={"chat_id":chat_id,"Nombre":username,"mensaje":texto}
-    resultado=base.post('Collection',datos)
-    return resultado
+    if not firebase_admin._apps:
+        base=firebase.FirebaseApplication("URL",None)
+        datos={"chat_id":chat_id,"Nombre":username,"mensaje":texto}
+        resultado=base.post('Collection',datos)
+        return resultado
+    else:
+        datos={"chat_id":chat_id,"Nombre":username,"mensaje":texto}
+        resultado=base.post('Collection',datos)
+        return resultado
