6cbed1de0550a7b738daadcb89e805d13b3e80ef
Erwin <75356078+Cuadernin@users.noreply.github.com>
2021-05-25 21:09:03 -0500

    Prueba

--- ConexionSegura.py
+++ ConexionSegura.py
@@ -1,7 +1,9 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
-
+"""
+Prueba de conexión a Firebase
+"""
 cred=credentials.Certificate('Collection')
 firebase_admin.initialize_app(cred,{'databaseURL':"URL"})
 ref=db.reference("/Collection/ERICK LEE GUZMÁN")
